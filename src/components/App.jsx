728cf7a54f013c49604825f9735113b34cf0a610
Kabelo <33831896+kmashao@users.noreply.github.com>
2020-09-21 23:20:23 +0200

    get time button updates time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  const [currentTime, setTime] = React.useState(time);
+
+  function getTime() {
+    setTime(() => {
+      return new Date().toLocaleTimeString();
+    });
+  }
+
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={getTime}>get time</button>
     </div>
   );
 }
